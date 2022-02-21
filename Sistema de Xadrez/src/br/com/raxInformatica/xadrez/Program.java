dba33e5ea9f84faf25e8a78db3c0c49770169ed1
RoAlencar <roalencarx@gmail.com>
2022-02-21 12:10:15 -0300

    First class : Position

--- Sistema de Xadrez/src/br/com/raxInformatica/xadrez/Program.java
+++ Sistema de Xadrez/src/br/com/raxInformatica/xadrez/Program.java
@@ -1,7 +1,12 @@
 package br.com.raxInformatica.xadrez;
 
+import br.com.raxInformatica.xadrez.boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Programa de xadrez");
+
+        Position position = new Position(3,5);
+
+        System.out.println(position);
     }
 }
